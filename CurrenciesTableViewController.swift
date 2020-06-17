cfa2434d3a47b8cc5b1bd87ba11532088c24aed4
DrewAndrew <andreyrusinovich1970@gmail.com>
2020-06-17 18:38:03 +0900

    works well

---
+++ CurrenciesTableViewController.swift
@@ -0,0 +1,68 @@
+//
+//  CurrenciesTableViewController.swift
+//  ForeignExchange
+//
+//  Created by Andrey Rusinovich on 11.06.2020.
+//  Copyright © 2020 Andrey Rusinovich. All rights reserved.
+//
+
+import UIKit
+
+class CurrenciesTableViewController: UITableViewController {
+    let currencies = ["RUB", "USD", "CAD", "EUR", "CZK", "GBP", "ISK", "MXN", "RON", "HKD", "PLN", "KRW", "PHP", "DKK", "HUF", "SEK", "IDR", "INR", "BRL"].sorted()
+    
+    var forConverting = false
+    var forResult = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return currencies.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "currencyCell", for: indexPath)
+
+        cell.textLabel?.text = ""
+        cell.detailTextLabel?.text = currencies[indexPath.row]
+        cell.imageView?.image = UIImage(named: currencies[indexPath.row] + "2x")
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let index = tableView.indexPathForSelectedRow!.row
+        
+        if segue.identifier == "returnToCurList" {
+            if forConverting {
+                ViewController.imageCurrencyToConvert = UIImage(named: currencies[index] + "1x")!
+                ViewController.baseCurrency = currencies[index]
+                forConverting = false
+            }
+            else if forResult {
+                ViewController.imageConvertedCurrency = UIImage(named: currencies[index] + "1x")!
+                ViewController.toConvertCurrency = currencies[index]
+                forResult = false
+            }
+        }
+    }
+}
+
---
+++ ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  ForeignExchange
+//
+//  Created by Andrey Rusinovich on 11.06.2020.
+//  Copyright © 2020 Andrey Rusinovich. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboard() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+class ViewController: UIViewController {
+    @IBOutlet var convertButton: UIButton!
+    
+    @IBOutlet var currencyToConvertButton: UIButton!
+    @IBOutlet var convertedCurrencyButton: UIButton!
+    
+    @IBOutlet var currencyToConvertTextFieled: UITextField!
+    @IBOutlet var convertedCurrencyLabel: UILabel!
+    
+    static var imageCurrencyToConvert = UIImage(named: "USD1x")
+    static var imageConvertedCurrency = UIImage(named: "RUB1x")
+    
+    static var baseCurrency = "USD"
+    static var toConvertCurrency = "RUB"
+    
+    @IBAction func convertButtonTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.7) {
+            self.convertButton.transform = CGAffineTransform.init(scaleX: 1.7, y: 1.7)
+            self.convertButton.transform = CGAffineTransform.init(scaleX: 1.0, y: 1.0)
+            
+            self.makeRequest()
+        }
+    }
+    
+    @IBAction func changeButtonTapped(_ sender: UIButton) {
+        swap(&ViewController.imageCurrencyToConvert, &ViewController.imageConvertedCurrency)
+        
+        swap(&ViewController.baseCurrency, &ViewController.toConvertCurrency)
+        
+        currencyToConvertButton.setImage(ViewController.imageCurrencyToConvert, for: .normal)
+        convertedCurrencyButton.setImage(ViewController.imageConvertedCurrency, for: .normal)
+        
+        makeRequest()
+    }
+    
+    func makeRequest() {
+        let baseURL = URL(string: "https://api.exchangeratesapi.io/latest?base=\(ViewController.baseCurrency)&symbols=\(ViewController.self.toConvertCurrency)")!
+        
+        let task = URLSession.shared.dataTask(with: baseURL) { (data, response, error) in
+            if let data = data,
+                let json = try? JSONSerialization.jsonObject(with: data, options: []) {
+                if let dictionary = json as? [String: Any],
+                    let convCurrency = dictionary["rates"] {
+                    let conv = (convCurrency as? [String: Any])![ViewController.self.toConvertCurrency] as! Double
+                    DispatchQueue.main.async {
+                        if let currentValue = Double(self.currencyToConvertTextFieled.text!) {
+                            let total = currentValue * conv
+                            self.convertedCurrencyLabel.text = String(format: "%.2f", total) + " " + ViewController.toConvertCurrency
+                        } else {
+                            self.convertedCurrencyLabel.text = String(0)
+                        }
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        convertButton.layer.cornerRadius = 7.0
+        
+        self.hideKeyboard()
+        
+        currencyToConvertButton.setImage(ViewController.imageCurrencyToConvert, for: .normal)
+        convertedCurrencyButton.setImage(ViewController.imageConvertedCurrency, for: .normal)
+
+        currencyToConvertTextFieled.layer.borderColor = UIColor.white.cgColor
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "currentCurrencySegue" {
+            let curTable = segue.destination as! CurrenciesTableViewController
+            curTable.forConverting = true
+        }
+        else if segue.identifier == "convertedCurrencySegue" {
+            let curTable = segue.destination as! CurrenciesTableViewController
+            curTable.forResult = true
+        }
+    }
+    
+    @IBAction func undwindToCurList(segue: UIStoryboardSegue) {
+        currencyToConvertButton.setImage(ViewController.imageCurrencyToConvert, for: .normal)
+        convertedCurrencyButton.setImage(ViewController.imageConvertedCurrency, for: .normal)
+    }
+}
+
